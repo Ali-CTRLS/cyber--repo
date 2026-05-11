f7210fe39d634263ecdecd2d9dbbfab5d21259d8
MoUmen-A <A.Ahmed64732@student.aast.edu>
2026-05-11 03:07:28 +0300

    Remove index.html and app.js files from the project

--- src/main/java/com/cyber/securefilesharing/repository/FolderTemplateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cyber.securefilesharing.repository;
-
-import com.cyber.securefilesharing.model.FolderTemplate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FolderTemplateRepository extends JpaRepository<FolderTemplate, Long> {
-}
--- src/main/java/com/cyber/securefilesharing/repository/ShareTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cyber.securefilesharing.repository;
-
-import com.cyber.securefilesharing.model.ShareToken;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShareTokenRepository extends JpaRepository<ShareToken, Long> {
-    Optional<ShareToken> findByToken(String token);
-}
